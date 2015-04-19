da78ab2424dbab481564d14604fdda75ae2e7298
ahl54 <anna.lu127@gmail.com>
2015-04-19 13:50:17 -0400

    newtab and window func

--- drexel_evohax/python scripts/web commands.py
+++ drexel_evohax/python scripts/web commands.py
@@ -13,24 +13,29 @@ import webbrowser
 def openbrowser():
     user_browser=webbrowser.get('windows-default')
     user_browser.open('http://www.google.com')
+
+# Opens a new tab
+def newtab():
+    user_browser=webbrowser.get('windows-default')
+    user_browser.open_new_tab(url + 'doc/')
     
+# Opens a new window
+def newwindow():
+    user_browser=webbrowser.get('windows-default')
+    webbrowser.open_new(url)
+   
 #TO DO:
     
 # Selectively reads content relevant to search engine and term
 def search(engine, term):
-    # opens engine such as wikipedia or google
-    # uses the appropriate engine to find relevant page
+    # opens input engine such as wikipedia or google
+    user_browser.open('http://www.' + engine + '.com')
+
+    # Fetch key board and mouse input using win32.dll
     # control + F to jump to relevant text content on page
 
-# Opens a new tab
-def newtab():
-    
-# Opens a new window
-def newwindow():
-    
-appfound = False;
-
 def openapp(app):
+    appfound = False;
     # Searches start menu for program
     # Check if app opens successfully
         #set appfound = True
